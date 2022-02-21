d279379f30d88ecd1b7835dd829a8ed494e6cbe5
Jonas Wagner <ltlygwayh@gmail.com>
2022-02-21 11:17:17 +0100

    Run pylint and fix many of the issues.
    The remaining ones seem minor or might go away with future refactoring.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 yxf (short for **Y**AML **X**LS**F**orms) is a converter between XLSForms and
 YAML files. With yxf, you can store forms as text-files. This brings a number of
-advantages to managing many forms. For example, users can store forms in version
-control or view differences between two forms.
+advantages to managing many forms. For example, you can store forms in version
+control, view differences between two forms, and easily share updates between
+multiple forms.
 
 ## Usage
 
@@ -11,7 +12,7 @@ To convert an XLSForm to a YAML file: `python -m yxf form.xlsx`.
 
 By default, the result will be called `form.yaml`, in other words, the same name
 as the input file with the extension changed to `.yaml`. You can specify a
-differnt output file name using the `--output othername.yaml` option.
+different output file name using the `--output othername.yaml` option.
 
 To convert a YAML file to an XLSForm: `python -m yxf form.yaml`.
 
@@ -30,14 +31,16 @@ python -m pip install 'git+https://github.com/Sjlver/yxf.git'
 ## Comments in forms
 
 yxf encourages adding comments to XLSForms. It uses a special column labeled `#`
-to that end. Other tools ignore this column, so that it can be used for whatever
+to that end. Other tools ignore this column, so that it can be used for
 explanations that are useful to the readers of the `.xlsx` or `.yaml` files.
 
 ## Development
 
 yxf is in an early stage of development. Feedback and contributions are welcome.
+Please open issues in the [issue tracker](https://github.com/Sjlver/yxf/issues).
 
 Please run all of the following before committing:
 
 - Format the code: `black .`
 - Run unit tests: `pytest`
+- See lint warnings (and please fix them :)): `pylint yxf`
--- yxf/__main__.py
+++ yxf/__main__.py
@@ -1,11 +1,24 @@
+"""
+yxf: Convert from XLSForm to YAML and back.
+
+To convert an XLSForm to a YAML file: `python -m yxf form.xlsx`.
+
+By default, the result will be called `form.yaml`, in other words, the same name
+as the input file with the extension changed to `.yaml`. You can specify a
+different output file name using the `--output othername.yaml` option.
+
+To convert a YAML file to an XLSForm: `python -m yxf form.yaml`.
+"""
+
 import argparse
 import collections
-import grapefruit
 import logging
+import pathlib
+
+import grapefruit
 import openpyxl
 import openpyxl.styles
 import openpyxl.utils
-import pathlib
 import strictyaml
 
 GROUP_COLOR = grapefruit.Color.NewFromHtml("#c7ffdb")
@@ -34,7 +47,7 @@ def _convert_sheet(sheet):
             if v is None:
                 continue
             if h is None:
-                raise ValueError("Cell with no column header: {}".format(v))
+                raise ValueError(f"Cell with no column header: {v}")
             row_dict[h] = v
         if row_dict:
             result.append(row_dict)
@@ -101,8 +114,8 @@ def _make_pretty_spreadsheet(wb):
                     width = max(len(w) for w in cell.value.splitlines())
                     widths[i].append(width)
         # We take the 75th percentile width plus 10
-        for i in range(len(widths)):
-            col_widths = sorted(widths[i])
+        for i, ws in enumerate(widths):
+            col_widths = sorted(ws)
             num_rows = len(col_widths)
             percentile_index = num_rows * 3 // 4
             estimated_width = col_widths[percentile_index] + 10
@@ -173,12 +186,12 @@ def _make_pretty_spreadsheet(wb):
 
 def _check_existing_output(filename, force):
     if filename.exists() and not force:
-        raise ValueError(
-            "File already exists (use --force to override): {}".format(filename)
-        )
+        raise ValueError(f"File already exists (use --force to override): {filename}")
 
 
 def xlsform_to_yaml(filename: pathlib.Path, target: pathlib.Path):
+    """Convert XLSForm file `filename` to YAML file `target`."""
+
     log.info("xlsform_to_yaml: %s -> %s", filename, target)
 
     wb = openpyxl.load_workbook(filename, read_only=True)
@@ -190,26 +203,24 @@ def xlsform_to_yaml(filename: pathlib.Path, target: pathlib.Path):
     first_line = result["survey"][0]
     if not "#" in first_line or not first_line["#"].startswith("Converted by yxf,"):
         first_line = {
-            "#": "Converted by yxf, from {}. Edit the YAML file instead of the Excel file.".format(
-                filename.name
-            )
+            "#": f"Converted by yxf, from {filename.name}. Edit the YAML file instead of the Excel file."
         }
         result["survey"].insert(0, first_line)
     elif first_line.get("#").startswith("Converted by yxf,"):
         first_line[
             "#"
-        ] = "Converted by yxf, from {}. Edit the YAML file instead of the Excel file.".format(
-            filename.name
-        )
+        ] = f"Converted by yxf, from {filename.name}. Edit the YAML file instead of the Excel file."
 
-    with open(target, "w") as f:
+    with open(target, "w", encoding="utf-8") as f:
         f.write(strictyaml.as_document(result).as_yaml())
 
 
 def yaml_to_xlsform(filename: pathlib.Path, target: pathlib.Path):
+    """Convert YAML file `filename` to XLSForm file `target`."""
+
     log.info("yaml_to_xlsform: %s -> %s", filename, target)
 
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         form = strictyaml.load(f.read()).data
 
     wb = openpyxl.Workbook()
@@ -250,7 +261,7 @@ def main():
         _check_existing_output(args.output, args.force)
         yaml_to_xlsform(args.file, args.output)
     else:
-        raise ValueError("Unrecognized file extension: {}".format(args.file))
+        raise ValueError(f"Unrecognized file extension: {args.file}")
 
 
 if __name__ == "__main__":
